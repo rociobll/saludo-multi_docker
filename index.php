8a21f30d788e6de3dd9e5a698663f28ad22e5a4e
Rocio <roball@alumnatflorida.es>
2025-02-11 18:02:21 +0100

    Segundo commit imagen docker saludo-multi

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Saludo aleatorio multilenguaje en dockerrr!!!</title>
+    <title>Saludo aleatorio multilenguaje en Dockerrr!!!</title>
     <style>
         body {
             font-family: Arial, sans-serif;
